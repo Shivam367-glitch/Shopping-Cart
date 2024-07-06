53e94e11d18707f5a7d6c29f9860f72c11023f3e
Shivam <txt2shivam@gmail.com>
2024-07-06 12:30:22 +0530

    Added LocalStorage

--- src/store/slices/cartSlice.js
+++ src/store/slices/cartSlice.js
@@ -1,6 +1,7 @@
 import {createSlice} from "@reduxjs/toolkit"; 
 import { toast } from "react-toastify";
-const initialState=[]; 
+const initialState=JSON.parse(localStorage.getItem('cartItems'))||[];  
+
 
 // const message=new toast();
 const cartSlice=createSlice({
@@ -8,13 +9,16 @@ const cartSlice=createSlice({
     initialState,
     reducers:{
       addToCart(state,action){
-        state.push(action.payload); 
+        state.push(action.payload);  
+        localStorage.setItem('cartItems',JSON.stringify(state));
         toast.success(`${action?.payload?.title} 
           is added to cart`);
       },
       deleteFromCart(state, action) { 
-        toast.error(`${action?.payload?.title} is removed from cart`);
-        return state.filter(item => item.id !== action.payload.id);
+        toast.error(`${action?.payload?.title} is removed from cart`); 
+        const filtered=state.filter(item => item.id !== action.payload.id); 
+        localStorage.setItem('cartItems',JSON.stringify(filtered));
+        return filtered;
       },
     }
 }); 
