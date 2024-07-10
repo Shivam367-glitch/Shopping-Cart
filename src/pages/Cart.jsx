474d0188ab1b02b81471df93f6d475938f39f38c
Shivam <txt2shivam@gmail.com>
2024-07-10 13:52:41 +0530

    Updated Cart Empty Message

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,8 @@
 
 import { useSelector,useDispatch } from "react-redux"; 
-import { incrementQuantity,deleteFromCart, decrementQuantity } from "../store/slices/cartSlice"; 
-// import ProductCard from "../components/Product-Card";  
+import { incrementQuantity,deleteFromCart, decrementQuantity } from "../store/slices/cartSlice";  
 import CartItems from "../components/Cart-Items";
+import { Link } from "react-router-dom";
 const Cart = () => {
   const state = useSelector((state) => state.cart); 
  const dispatch=useDispatch();
@@ -17,11 +17,13 @@ const handleDecrement=(itemId)=>{
 
 const handleDelete=(itemId)=>{
   dispatch(deleteFromCart(itemId));
-}
+} 
+
+
   return (
     <>
       <div className="container-fluid  mt-5 mb-3">
-       <div className="row d-flex flex-column flex-md-row justify-content-between justify-content-md-center justify-content-lg-between align-items-sm-center align-items-md-baseline w-100">
+       <div className="row d-flex flex-column flex-md-row justify-content-between justify-content-md-center justify-content-lg-between align-items-center align-items-md-baseline w-100">
        <div className="col-12 col-sm-8 col-md-8 col-lg-8   text-center">
        {
           state.items.length>0 ?state.items.map((item,index)=>{
@@ -31,16 +33,22 @@ const handleDelete=(itemId)=>{
               </div>
              )
           })
-         
-          :<h2>Cart Is Empty</h2>
+          :
+          <div className="col-12 d-flex flex-column gap-3 text-center   ">
+            <h1>Your Shopping Cart is empty</h1>
+            <p>Looks like you haven’t added any items to the cart yet.</p>
+            <Link className="d-block mx-auto rounded border border-transparent  bg-dark px-5 py-3 text-center text-decoration-none text-white" to="/">Explore products</Link>
+          </div>
         }
        </div> 
-       <div className="col-12 col-md-4 col-lg-3 border rounded-2 ms-3 ms-lg-0  mt-4 d-flex flex-column justify-content-center  align-self-center align-self-md-baseline" style={{maxHeight:'200px'}} > 
+       {
+        state.items.length>0?<div className="col-12 col-md-4 col-lg-3 border rounded-2 ms-3 ms-lg-0  mt-4 d-flex flex-column justify-content-center  align-self-center align-self-md-baseline" style={{maxHeight:'200px'}} > 
         <div className="d-flex flex-row justify-content-between mt-2"><span>SubTotal</span><span>${Math.round(state.totalAmount)}</span></div>
         <div className="d-flex flex-row justify-content-between mt-2"><span>Shipping</span><span>$0</span></div> 
         <div className="d-flex flex-row justify-content-between mt-2"><span>Total</span><span>${Math.round(state.totalAmount)}</span></div>
         <button className="rounded-2 mt-5 checkout-btn align-self-center mb-2">Checkout</button>
-       </div>
+       </div>:null
+       }
        </div> 
       
       </div>
